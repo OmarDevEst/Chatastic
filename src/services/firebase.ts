f9cd7e0a8a2f784ad1f4d39d49580765862cd9cb
ckansito981@gmail.com <ckansito981@gmail.com>
2021-05-13 19:04:59 -0500

    Chat final

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -2,23 +2,30 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const {REACT_APP_APIKEY,
+/*const{  REACT_APP_APPIKEY,
         REACT_APP_AUTDOMAIN,
         REACT_APP_PROJECTID,
         REACT_APP_STORAGEBUCKET,
         REACT_APP_MESSAGINSENDERID,
         REACT_APP_APPID,
-        REACT_APP_MEASURENTID
-}=process.env;
+        REACT_APP_MEASURENTID,
+}=process.env; Proceso por seguridad*/
 
 firebase.initializeApp({
-        apiKey: REACT_APP_APIKEY,
-        authDomain: REACT_APP_AUTDOMAIN,
-        projectId:REACT_APP_PROJECTID,
-        storageBucket:REACT_APP_STORAGEBUCKET,
-        messagingSenderId: REACT_APP_MESSAGINSENDERID,
-        appId: REACT_APP_APPID,
-        measurementId: REACT_APP_MEASURENTID
+        apiKey: "AIzaSyCp4LKDFSoJRHaexG-hNZTVRemcrSGuHVk",
+        //apiKey:REACT_APP_APPIKEY
+        authDomain: "chat-9aa67.firebaseapp.com",
+        //authDomain:REACT_APP_AUTDOMAIN
+        projectId: "chat-9aa67",
+        //projectId:REACT_APP_PROJECTID
+        storageBucket: "chat-9aa67.appspot.com",
+        //storageBucket: REACT_APP_STORAGEBUCKET
+        messagingSenderId: "1035068824341",
+        //messagingSenderId: REACT_APP_MESSAGINSENDERID,
+        appId: "1:1035068824341:web:95e6c5fd3cdef24129271c",
+        //appId:REACT_APP_APPID,
+        measurementId: "G-WQ866YN7T6"
+        //measurementId:REACT_APP_MEASURENTID,
 });
 
 const auth = firebase.auth();
